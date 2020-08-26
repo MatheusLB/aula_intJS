1e28f33db6b9821cfd3ba41d127da544af8031a5
MatheusLB <matheuslb09@gmail.com>
2020-08-26 11:09:28 -0300

    Add files via upload

--- <js/main.js
+++ <js/main.js
@@ -1,10 +1,38 @@
-function soma(n1,n2){
+function soma(n1,n2)
+{
+
 
 
 return n1+n2;
 
 
+
 }
 
 
-alert(soma(5,10));
+
+//alert(soma(5,10));
+
+
+
+function validar (idade){
+
+var validar;
+
+if (idade >=18){
+
+validar = true;
+
+}else{
+
+validar = false;
+
+}
+return validar;
+}
+
+
+var idade = prompt("Digite sua idade");
+
+console.log(validar(idade));
+
